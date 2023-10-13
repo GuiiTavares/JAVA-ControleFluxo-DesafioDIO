57be65e10c9d6d53e34f8e80668781483752649a
Guilherme Tavares <gui8311@hotmail.com>
2023-10-13 17:47:25 -0300

    first commit

---
+++ src/Contador/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package Contador;
+
+public class ParametrosInvalidosException extends Exception {
+    public static void main(String[] args) {
+        String mensagemDeErro = "O segundo parâmetro deve ser maior que o primeiro";
+      }
+}
